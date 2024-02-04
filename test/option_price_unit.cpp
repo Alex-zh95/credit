5563452a35b206cb35ab4c1f0de4041c475ea3e9
chu <zh.c.l.z.95@gmail.com>
2024-02-04 14:04:33 +0000

    Put and dividends tested

--- test/option_price_unit.cpp
+++ test/option_price_unit.cpp
@@ -17,18 +17,24 @@ double volatility = 0.10;
 double duration = 0.75;
 
 double call_price, call_phi1, call_phi2;
+double dividend = 0.001;  // For the put only
+double put_price, put_phi1, put_phi2;
 
 // Values calculated in Python - we expect these to be returned.
 double expected_call_price = 0.18199750501184386;
-double expected_Phi1 = 0.05166137589623915;
-double expected_Phi2 = 0.04312161692483895;
+double expected_call_Phi1 = 0.05166137589623915;
+double expected_call_Phi2 = 0.04312161692483895;
+
+double expected_put_price = 15.836460562018175;
+double expected_put_Phi1 = 0.949248908843968;
+double expected_put_Phi2 = 0.9576656456260665;
 
 int main() {
     std::cout << "Testing function risk_neutral::option_price." << std::endl;
 
     option_price(init, strike, interest, volatility, duration, call_price, call_phi1, call_phi2);
 
-    std::cout << "Option price  =    " << call_price << std::endl;
+    std::cout << "Call price    =    " << call_price << std::endl;
     std::cout << "Phi1          =    " << call_phi1 << std::endl;
     std::cout << "Phi2          =    " << call_phi2 << std::endl;
 
@@ -37,12 +43,32 @@ int main() {
     else
         std::cout << "Check call price!!\n";
 
-    if (abs(call_phi1 - expected_Phi1) <= TOL)
+    if (abs(call_phi1 - expected_call_Phi1) <= TOL)
         std::cout << "Phi1 as expected" << std::endl;
     else
         std::cout << "Check Phi1!!\n";
 
-    if (abs(call_phi2 - expected_Phi2) <= TOL)
+    if (abs(call_phi2 - expected_call_Phi2) <= TOL)
+        std::cout << "Phi2 as expected" << std::endl;
+    else
+        std::cout << "Check Phi2!!\n";
+
+    option_price(init, strike, interest, volatility, duration, put_price, put_phi1, put_phi2, false, dividend);
+    std::cout << "Put price     =   " << put_price << std::endl;
+    std::cout << "Phi1          =    " << put_phi1 << std::endl;
+    std::cout << "Phi2          =    " << put_phi2 << std::endl;
+
+    if (abs(put_price - expected_put_price) <= TOL)
+        std::cout << "Put price as expected" << std::endl;
+    else
+        std::cout << "Check put price!!\n";
+
+    if (abs(put_phi1 - expected_put_Phi1) <= TOL)
+        std::cout << "Phi1 as expected" << std::endl;
+    else
+        std::cout << "Check Phi1!!\n";
+
+    if (abs(put_phi2 - expected_put_Phi2) <= TOL)
         std::cout << "Phi2 as expected" << std::endl;
     else
         std::cout << "Check Phi2!!\n";
