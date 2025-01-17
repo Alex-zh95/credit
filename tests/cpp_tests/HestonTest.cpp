676020248370fc6addd144f0b168096138ce8c75
chu <zh.c.l.z.95@gmail.com>
2025-01-17 08:45:38 +0000

    Heston volatility test Python vs C++

--- tests/cpp_tests/HestonTest.cpp
+++ tests/cpp_tests/HestonTest.cpp
@@ -5,9 +5,6 @@
 #include <vector>
 #include "../../src/credit/stvol.hpp"
 
-using std::pow;
-using std::sqrt;
-
 bool test_Heston_pricing()
 {
     auto U = std::make_unique<StVol::Underlying>();
@@ -47,69 +44,24 @@ bool test_Heston_pricing()
     return (std::abs(error) < 1e-3);
 }
 
-bool test_Heston_calibration()
+void basic_Heston_calibration_test()
 {
-    std::vector<StVol::HestonCallMdl> mdls;
-    std::vector<double> optnPrices;
+    const auto S0 = 228.26;
 
-    // Generate some artifical option prices
-    std::cout << "Generating artificial option prices...\n";
+    std::vector<double> Tau = {0.02, 0.02, 0.02, 0.04, 0.057};
+    std::vector<double> Ks = {115, 120, 130, 115, 185};
+    std::vector<double> Ps = {113.4, 108.4, 98.4, 114.6, 44.5};
+    std::vector<double> Rfs = {0.0446, 0.0446, 0.0446, 0.0444, 0.0443};
 
-    // Expected values
-    auto S0 = 100.;
-    auto v0 = 0.1;
-    auto alpha = 1.5768;
-    auto vTheta = 0.0398;
-    auto vLambda = 0.575;
-    auto vSig = 0.3;
-    auto rho = -0.5711;
-
-    // Set up initial values (expected values with some shifts)
-    std::vector<double> x0 = {v0+0.05, alpha-0.01, vTheta+0.02, vSig+0.05, vLambda-0.01, rho+0.001, 0.03};
-
-    for (int j = -10; j < 10; j++) 
-    {
-        auto dj = static_cast<double>(j);
-        auto U = std::make_unique<StVol::Underlying>();
-        U->S0 = S0;
-        U->v0 = x0[0] * (1 + dj/100);
-        U->alpha = x0[1];
-        U->vTheta = x0[2] * (1 + 2*dj/100);
-        U->vSig = x0[3] * (1 + dj/100);
-        U->vLambda = x0[4] * (1 + 2*dj/100);
-        U->rho = x0[5] * (1 + dj/100);
-        U->rf = x0[6];
-        auto strike = 100.;
-
-        StVol::HestonCallMdl mdl(std::move(U), strike);
-        mdl.calc_option_price();
-        auto mdl_price = mdl.get_option_price();
-        
-        mdls.push_back(std::move(mdl));
-        optnPrices.push_back(mdl_price);
-    }
-    std::cout << "Optimization step...\n";
-
-    // Using the above, try to fit a Heston model to see if we recover params
-    auto V = StVol::market_calibration(mdls, optnPrices, x0);
-    std::cout << "Fitted result:\n";
-    std::cout << "Spot price:            " << V->S0 << "\n";
-    std::cout << "Spot volatility:       " << V->v0 << "\n";
-    std::cout << "Risk-free rate:        " << V->rf << "\n";
-    std::cout << "Mean-reversion rate:   " << V->alpha << "\n";
-    std::cout << "Long-term mean var:    " << V->vTheta << "\n";
-    std::cout << "Volatility volatility: " << V->vSig << "\n";
-    std::cout << "Market price of vol:   " << V->vLambda << "\n";
-    std::cout << "Correlation vol/price: " << V->rho << "\n";
-
-    auto error2 = pow(V->v0 - v0, 2) / v0 + pow(V->alpha - alpha, 2) / alpha + pow(V->vTheta - vTheta, 2) / vTheta + pow(V->vLambda - vLambda, 2) / vLambda + pow(V->vSig - vSig, 2) / vSig + pow(V->rho - rho, 2) / rho;
-
-    std::cout << "Avg percentage error:  " << sqrt(error2) * 100 / x0.size() << "%\n";
-
-    StVol::HestonCallMdl mdl(std::move(V), 100.0);
-    std::cout << "Fitted risk-neutral P2:" << mdl.get_rn_exercise_probability() << "\n";
-
-    return (std::abs(sqrt(error2)) < 0.1 * x0.size());
+    auto U = StVol::fitHeston(S0, Ks, Rfs, Tau, Ps);
+    std::cout << "Spot price:            " << U->S0 << "\n";
+    std::cout << "Spot volatility:       " << U->v0 << "\n";
+    std::cout << "Risk-free rate:        " << U->rf << "\n";
+    std::cout << "Mean-reversion rate:   " << U->alpha << "\n";
+    std::cout << "Long-term mean var:    " << U->vTheta << "\n";
+    std::cout << "Volatility volatility: " << U->vSig << "\n";
+    std::cout << "Market price of vol:   " << U->vLambda << "\n";
+    std::cout << "Correlation vol/price: " << U->rho << "\n";
 }
 
 int main() 
@@ -120,11 +72,8 @@ int main()
     testPass &= test_Heston_pricing();
     std::cout << "\ntest_Heston_pricing result: " << (testPass?"Pass":"Fail") << "\n\n";
 
-    // Final result
-    std::cout << "Running test_Heston_calibration...\n\n";
-    testPass &= test_Heston_calibration();
-    std::cout << "\ntest_Heston_calibration result: " << (testPass?"Pass":"Fail") << "\n\n";
-    std::cout << "All tests result: " << (testPass?"Pass":"Fail") << "\n";
-
+    // std::cout << "All tests result: " << (testPass?"Pass":"Fail") << "\n";
+    std::cout << "Testing calibration...\n";
+    basic_Heston_calibration_test();
     return (testPass?0:1);
 }
--- tests/py_tests/hestonVol.py
+++ tests/py_tests/hestonVol.py
@@ -5,23 +5,34 @@
 
 from src.pyvol import cpy_credit as cc
 from src.pyvol.heston import get_call_information
+from src.pyvol.heston import fit_heston as py_fit_Heston
 
 # Get latest share information and collection of live traded options
+print('Getting information on AAPL')
 S0, vol_surf = get_call_information('AAPL')
+
+print(f'Spot price of AAPL:     {S0:,.2f}\n')
 
 # Remove any 0-options
 vol_surf = vol_surf[vol_surf['price'] > 0]
+vol_surf = vol_surf[vol_surf['maturity'] > 0]
+print('Snippet of volatility surface data:')
+print(vol_surf.head())
 
-# Fit Heston model
-pyV = cc.fit_Hst(S0, vol_surf['strike'], vol_surf['maturity'], vol_surf['price'])
+# Fit Heston model in C++
+pyV = cc.fit_Heston(S0, vol_surf['strike'].values, vol_surf['rf'].values, vol_surf['maturity'].values, vol_surf['price'].values)
 
-print('\nFit results:')
-print(f'Spot price:         {pyV.S0:,.1f}')
-print(f'Spot volatility:    {pyV.v0:,.1%}')
-print(f'Risk-free rate:     {pyV.rf:,.1%}')
-print(f'Mean-rev rate:      {pyV.alpha:,.1f}')
-print(f'Asymp mean var:     {pyV.vTheta:,.1%}')
-print(f'Vol of vol:         {pyV.vSig:,.1%}')
-print(f'Markt price of vol: {pyV.vLambda:,.1%}')
-print(f'Price/vol corr:     {pyV.rho:,.1%}')
+print('\nFit results (C++):')
+print(f'Spot volatility (v0):           {pyV.v0:,.1%}')
+print(f'Mean-rev rate (kappa):          {pyV.alpha:,.1f}')
+print(f'Asymp mean var (theta):         {pyV.vTheta:,.1%}')
+print(f'Vol of vol (sigma):             {pyV.vSig:,.1%}')
+print(f'Markt price of vol (lambda):    {pyV.vLambda:,.1%}')
+print(f'Price/vol corr (rho):           {pyV.rho:,.1%}')
 print('\n')
+
+# Fit Heston model in Python
+print('Fit results (Python):')
+resultHeston = py_fit_Heston(vol_surf, S0)
+for key, item in resultHeston.items():
+    print(f'{key}: {item}')
